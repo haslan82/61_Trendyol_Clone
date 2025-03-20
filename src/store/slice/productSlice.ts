d29e0ba28d88d48a7299aa0e8676ad3888677ed2
haslan82 <haslan82@hotmail.com>
2025-03-21 00:27:10 +0300

    update

--- src/store/slice/productSlice.ts
+++ src/store/slice/productSlice.ts
@@ -1,73 +1,71 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {ProductsState} from '../../models/data/productsState';
-import { getAllProducts, getBestSellerProducts, getPopularProducts, getProductDetail } from '../actions/productsActions';
-
-
+import {
+  getAllProducts,
+  getBestSellerProducts,
+  getPopularProducts,
+  getProductDetail,
+} from '../actions/productsActions';
 const initialState: ProductsState = {
   products: [],
   bestSellerProducts: [],
   popularProducts: [],
-  product:{},
-  pending:false,
+  product: {},
+  pending: false,
   error: null,
 };
-
 export const productSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
     addFavoriteOther: (state, action) => {
       const product = action.payload;
-      const exitingProduct = state.bestSellerProducts.find(item => item.id === product.id);
-      if (exitingProduct) {
-        exitingProduct.isFavorite=!exitingProduct.isFavorite;
-        // state.bestSellerProducts = state.bestSellerProducts.filter(item => item.id !== action.payload.id);
-      }  // else {
-      //    state.bestSellerProducts.push({...product, isFavorite: true});
-        
-      // } 
-      console.log( exitingProduct)
-    }
-  
+      const exitingProductBestSeller = state.bestSellerProducts.find(
+        item => item.id === product.id,
+      );
+      const exitingProductPopuler = state.popularProducts.find(
+        item => item.id === product.id,
+      );
+      const exitingAllproduct = state.products.find(
+        item => item.id === product.id,
+      );
+      if (exitingAllproduct) {
+        exitingAllproduct.isFavorite = !exitingAllproduct.isFavorite;
+      }
+      if (exitingProductBestSeller) {
+        exitingProductBestSeller.isFavorite =
+          !exitingProductBestSeller.isFavorite;
+      }
+      if (exitingProductPopuler) {
+        exitingProductPopuler.isFavorite = !exitingProductPopuler.isFavorite;
+      }
+      if (product.id === state.product.id) {
+        state.product.isFavorite = !state.product.isFavorite;
+      }
+    },
   },
   extraReducers: builder => {
     builder
-      /*  .addCase(getBestSellerProducts.pending, (state, action) => {}) */
-
       .addCase(getAllProducts.pending, (state, action) => {
         state.pending = true;
       })
-
       .addCase(getAllProducts.fulfilled, (state, action) => {
         state.products = action.payload;
         state.pending = false;
       })
-
-
+      .addCase(getAllProducts.rejected, (state, action) => {
+        state.pending = false;
+        state.error = action.error;
+      })
       .addCase(getBestSellerProducts.fulfilled, (state, action) => {
         state.bestSellerProducts = action.payload;
       })
-
-      .addCase(getAllProducts.rejected, (state, action) => {
-        state.pending=false;
-        state.error=action.error
-       
+      .addCase(getPopularProducts.fulfilled, (state, action) => {
+        state.popularProducts = action.payload;
       })
-
-
-
-    /*  .addCase(getBestSellerProducts.rejected, (state, action) => {}); */
-
-
-        .addCase(getPopularProducts.fulfilled, (state, action) => {
-          state.popularProducts = action.payload;
-        })
-
-        .addCase(getProductDetail.fulfilled, (state, action) => {
-          state.product = action.payload;
-        });
-
-
+      .addCase(getProductDetail.fulfilled, (state, action) => {
+        state.product = action.payload;
+      });
   },
 });
 export const {addFavoriteOther} = productSlice.actions;
