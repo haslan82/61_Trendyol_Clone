519c7b73fd86851018d71e640b63cf91db367ea1
haslan82 <haslan82@hotmail.com>
2025-03-24 23:49:31 +0300

    update

--- src/store/actions/authAction.ts
+++ src/store/actions/authAction.ts
@@ -1,13 +1,21 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {postRequest} from '../../service/verbs';
 import {AUTH_URLS} from '../../service/urls';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 const userLogin = createAsyncThunk('auth/userLogin', async (params: object) => {
    // console.log(params)
    try {
       const response = await postRequest(params, AUTH_URLS.LOGIN);
-      //console.log(response.data) 
+     // console.log(response.data) 
+  if(response.data && response.data.token){
+    await AsyncStorage.setItem('token', response.data.token);
+   
+  }
  return response.data;
+ 
 
    } catch (error) {
   //  console.log('error: ' + JSON.stringify(error))
