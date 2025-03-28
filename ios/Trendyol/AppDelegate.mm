ddb74357c9f5f4bb9e12c2ee070a60d16f294222
haslan82 <haslan82@hotmail.com>
2025-03-28 23:37:10 +0300

    update

--- ios/Trendyol/AppDelegate.mm
+++ ios/Trendyol/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-import FirebaseCore
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,8 +10,7 @@ import FirebaseCore
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  FirebaseApp.configure()
-
+  [FIRApp configure];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
