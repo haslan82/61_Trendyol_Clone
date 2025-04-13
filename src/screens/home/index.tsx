144bf504903abed68b85d3c616050f800164732a
haslan82 <haslan82@hotmail.com>
2025-04-13 21:29:40 +0300

    update

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Categories from '../../widgets/categories';
-import {SafeAreaView, ScrollView, View} from 'react-native';
-import {defaultScreenStyle} from '../../styles/defaultScreenStyle';
+import { SafeAreaView, ScrollView, View } from 'react-native';
+import { defaultScreenStyle } from '../../styles/defaultScreenStyle';
 import Introduction from '../../widgets/introduction';
 import BestSeller from '../../widgets/bestSeller';
 import PopularProducts from '../../widgets/popularProducts';
+import messaging from '@react-native-firebase/messaging';
+
 
 
 const Home: React.FC = () => {
-  return (
-    <SafeAreaView style={defaultScreenStyle.safeAreaContainer}>
-      <ScrollView 
-      showsVerticalScrollIndicator={false}>
-        <View style={defaultScreenStyle.container}>
-          <Categories />
-          <Introduction />
-          <BestSeller />
-          <PopularProducts />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  };
+
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
+  
+return (
+  <SafeAreaView style={defaultScreenStyle.safeAreaContainer}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}>
+      <View style={defaultScreenStyle.container}>
+        <Categories />
+        <Introduction />
+        <BestSeller />
+        <PopularProducts />
+      </View>
+    </ScrollView>
+  </SafeAreaView>
+);
+};
 
 export default Home;
