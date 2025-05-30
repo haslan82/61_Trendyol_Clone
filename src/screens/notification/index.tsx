44ea05ad41832d928b9293a8ca9ab69ce9c40ddc
haslan82 <haslan82@hotmail.com>
2025-05-30 18:49:47 +0300

    update

--- src/screens/notification/index.tsx
+++ src/screens/notification/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { defaultScreenStyle } from '../../styles/defaultScreenStyle';
+import { Colors } from '../../themes/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 
@@ -9,10 +11,19 @@ import { defaultScreenStyle } from '../../styles/defaultScreenStyle';
 const Notifications
     : React.FC = () => {
         return (
-            <View style={defaultScreenStyle.safeAreaContainer}>
-                <Text style={{ fontSize: 30 }}> Notifications
-                </Text>
+            <View style={defaultScreenStyle.container}>
+                <View style={styles.notiBox}>
+               <View>
+               <Text style={{ fontSize: 30 }}>İndirim kuponu hesabınıza tanımlanmıştır.
+               </Text>
+               </View>
+                
+                <View>
+                    <Icon name="notifications-outline" size={30} color={Colors.primary} />
+                </View>
+            </View> 
             </View>
+           
         );
     };
 
@@ -20,11 +31,28 @@ const Notifications
 const styles = StyleSheet.create({
 
     container: {
+        flexDirection: 'row',
+        backgroundColor: Colors.primary1,
         alignItems: 'center',
         flex: 1,
-        justifyContent: 'center'
-    }
+        justifyContent: 'space-evenly',
+        borderWidth: 1,
+        borderColor: Colors.gray,
+        marginTop:10,
+       
+    },
+    notiBox: {
+        flexDirection: 'row',
+        backgroundColor: Colors.primary1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderWidth: 1,
+        borderColor: Colors.gray,
+        marginTop:10,
+        borderRadius:10,
+        paddingLeft:10
+    },
+    
 });
 
-export default Notifications
-    ;
+export default Notifications;
--- src/store/actions/categoriesActions.ts
+++ src/store/actions/categoriesActions.ts
@@ -13,4 +13,5 @@ return response.data
 );
 
 
+
 export {getAllCategories};
